62fa3c16e7012a4aa7283db8dbb441e5c29e18ae
Erik Vardanyan <erikfcb@gmail.com>
2019-02-13 16:33:05 +0200

    first commit

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -14,6 +14,9 @@ class Board extends Component {
     this.nextTurn = this.nextTurn.bind(this);
     this.lastAvailableRow = this.lastAvailableRow.bind(this);
     this.checkWinner = this.checkWinner.bind(this);
+    this.includedAtLast = this.includedAtLast.bind(this);
+    this.includedAtFirst = this.includedAtFirst.bind(this);
+    this.vertical = this.vertical.bind(this);
   }
 
   componentDidMount() {
@@ -28,14 +31,11 @@ class Board extends Component {
       const row = [];
       for (let j = 0; j < 7; j++) {
         row.push({
-          element: (
-            <td onClick={() => this.changeColor(j)}>
-              <div className="spot" key={counter}>
-                {counter}
-              </div>
-            </td>
-          ),
-          selected: ""
+          elementClass: "spot ",
+          selected: "",
+          index: counter,
+          column: j,
+          row: i
         });
         counter++;
       }
@@ -61,21 +61,142 @@ class Board extends Component {
     return lastRow;
   }
 
-  vertical(arr) {}
-  horizontal(arr) {}
-  diagonal(arr) {}
+  includedAtLast(arr) {
+    // Checks if 'forbidden' numbers appear except last index of an array
+    const forbidden = [];
+    this.state.board.forEach((element, index, arr) => {
+      forbidden.push(index * arr[index].length + arr.length);
+    });
+
+    let flag = false;
+
+    arr.forEach((x, index) => {
+      if (index !== arr.length - 1 && forbidden.indexOf(x) !== -1) {
+        flag = true;
+      }
+    });
+
+    return flag;
+  }
+
+  includedAtFirst(arr) {
+    console.log('given array: '+arr)
+    // Checks if 'forbidden' numbers appear except first index of an array
+    const forbidden = [];
+    this.state.board.forEach((element, index) => {
+      forbidden.push(index * element.length);
+    });
+
+    let flag = false;
+
+    arr.forEach((x, index) => {
+      if (index !== arr.length -1 && forbidden.indexOf(x) !== -1) {
+        flag = true;
+      }
+    });
+
+    return flag;
+  }
+
+  vertical(arr) {
+    const rowLength = this.state.board[0].length;
+    let found = false;
+
+    arr.forEach((num, index) => {
+      let gap = 0;
+      let sequenceLength = 0;
+      let root = index;
+
+      if (arr.length - index > 3)
+        while (gap <= rowLength) {
+          gap = arr[index + 1] - arr[root];
+          if (gap == rowLength) {
+            sequenceLength++;
+            root = index + 1;
+          }
+          index++;
+        }
+      if (sequenceLength == 3) found = true;
+    });
+
+    return found;
+  }
+
+  horizontal(arr) {
+    let found = false;
+
+    arr.forEach((num, index) => {
+      let sequence = [num];
+      let gap = arr[index + 1] - num;
+
+      while (gap == 1) {
+        if (gap == 1) {
+          sequence.push(arr[index + 1]);
+          index++;
+          num++;
+        }
+        gap = arr[index + 1] - num;
+      }
+
+      if (sequence.length == 4 && !this.includedAtLast(sequence)) found = true;
+    });
+    return found;
+  }
+
+  diagonal(arr) {
+    const bigGap = this.state.board[0].length + 1;
+    const smallGap = this.state.board[0].length - 1;
+    let found = false;
+
+    arr.forEach((num, index) => {
+      let gap = 0;
+      let sequence = [num];
+      let root = index;
+
+      if (arr.length - index > 3)
+        while (gap <= bigGap) {
+          gap = arr[index + 1] - arr[root];
+          if (gap == bigGap) {
+            sequence.push(arr[index + 1]);
+            root = index + 1;
+          }
+          index++;
+        }
+      if (sequence.length == 4 && !this.includedAtLast(sequence)) found = true;
+    });
+
+    arr.forEach((num, index) => {
+      let gap = 0;
+      let sequence = [num];
+      let root = index;
+
+      if (arr.length - index > 3)
+        while (gap <= smallGap) {
+          gap = arr[index + 1] - arr[root];
+          if (gap == smallGap) {
+            sequence.push(arr[index + 1]);
+            root = index + 1;
+          }
+          index++;
+        }
+      if (sequence.length == 4 && !this.includedAtFirst(sequence)) found = true;
+    });
+
+    return found;
+  }
 
   checkWinner() {
     let sequence = [];
 
     this.state.board.forEach((el, row) => {
       el.forEach((x, column) => {
-        if (x.selected === this.state.turn) sequence.push({ row, column });
+        if (x.selected === this.state.turn) sequence.push(x.index);
       });
     });
 
+    console.log(sequence);
     if (
-      sequence.length < 3 &&
+      sequence.length > 3 &&
       (this.vertical(sequence) ||
         this.horizontal(sequence) ||
         this.diagonal(sequence))
@@ -85,39 +206,70 @@ class Board extends Component {
 
   changeColor(column) {
     const rowToPlace = this.lastAvailableRow(column);
-    console.log("row: " + rowToPlace + "col: " + column);
     if (rowToPlace !== -1) {
-      this.state.board[rowToPlace][column].element = (
-        <td onClick={() => this.changeColor(column)}>
-          <div
-            className={"spot insert " + this.state.turn}
-            key={rowToPlace * column}
-          />
-        </td>
-      );
+      let counter = -1;
+      const board = this.state.board.map((element, index) => {
+        if (index !== rowToPlace) return element;
+        let newRow = element.map(el => {
+          counter++;
+          if (el.column !== column) return el;
 
-      this.state.board[rowToPlace][column].selected = this.state.turn;
-      this.checkWinner();
-      this.nextTurn();
+          return {
+            elementClass: "spot insert " + this.state.turn,
+            selected: this.state.turn,
+            index: this.state.board[rowToPlace][column].index,
+            column: column,
+            row: rowToPlace
+          };
+        });
+
+        return newRow;
+      });
+
+      console.log(this.state.board);
+
+      console.log(board);
+
+      this.setState({ board });
+      setTimeout(() => {
+        this.checkWinner();
+        this.nextTurn();
+      }, 100);
     }
   }
 
   getSpots() {
-    // returns all divs from 'board' array to dispaly as game board
+    // creates divs from 'board' array to dispaly as game board
     let arr = [];
     this.state.board.forEach((element, column) => {
       element.forEach(el => {
-        arr.push(el.element);
+        arr.push(
+          <td onClick={() => this.changeColor(el.column)}>
+            <div className={el.elementClass} key={el.index} />
+          </td>
+        );
       });
     });
 
     return arr;
   }
 
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   render() {
-    const spots = this.getSpots();
+    // // const arr = [0,7,14,21,28,35]
+    // const arr = [0,2,4,6,8]    
+    // console.log(arr);
+    // console.log(this.includedAtFirst(arr));
 
-    return <table className="board">{spots}</table>;
+    const spots = this.getSpots();
+    const turn = this.capitalizeFirstLetter(this.state.turn)
+
+    return [
+      <div className="turn">{turn}'s turn</div>,
+      <table className="board">{spots}</table>
+    ];
   }
 }
 
